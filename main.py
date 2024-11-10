01e66d4f4e42ae906222682cba791b1c9a5ad8d0
ldamborena <ldamborena@correo.um.edu.uy>
2024-11-09 23:03:06 -0300

    main file
    test

---
+++ main.py
@@ -0,0 +1,54 @@
+from static.menus import options
+from static.Gremio import Gremio
+from exceptions.optionException import mainMenuException
+
+def action(opcion_a, gremio):
+    if opcion_a == "4":
+        while True:
+            print(options["otherOptions"])
+            opcion_b = input("Seleccione una opcion: ")
+            if not opcion_b.isdigit() or int(opcion_b) != "4" or int(opcion_b) not in range(1, 6):
+                raise mainMenuException("Opción inválida, elija una de las opciones mostradas.")
+            elif opcion_b == "5":
+                break
+            else:
+                print("Opcion elegida: ", opcion_b)
+    elif opcion_a == "1":
+        while True:
+            print(options["selectGuerrero"])
+            opcion_b = input("Seleccione una opcion: ")
+            if not opcion_b.isdigit() or int(opcion_b) not in range(1, 4):
+                raise mainMenuException("Opción inválida, elija una de las opciones mostradas.")
+            else:
+                clase = {"1": "Guerrero","2": "Mago","3": "Ranger"}
+                nombre = input("Ingrese el nombre del aventurero: ")
+                id = input("Ingrese el id del aventurero: ")
+                puntos_habilidad = input("Ingrese los puntos de habilidad del aventurero: ")
+                experiencia = input("Ingrese la experiencia del aventurero: ")
+                dinero = input("Ingrese el dinero del aventurero: ")
+                atributos_adicionales = input("Ingrese los atributos adicionales del aventurero: ")
+                gremio.registrar_aventurero(nombre, int(id), int(puntos_habilidad), int(experiencia), int(dinero), clase, atributos_adicionales)
+                print("Aventurero registrado con éxito!")
+                print(gremio.aventureros)
+                break
+
+
+def main():
+    gremio_actual = Gremio()
+    while True:
+        print(options["main"])
+        opcion = input("Seleccione una opcion: ")
+        try:
+            if not opcion.isdigit() or int(opcion) not in range(1, 6):
+                raise mainMenuException("Opción inválida, elija una de las opciones mostradas.")
+            elif opcion == "5":
+                print(options["end"])
+                break
+            else:
+                action(opcion, gremio_actual)
+        except mainMenuException as e:
+            print(e.message)
+            continue
+
+if __name__  == "__main__":
+    main()
---
+++ static/menus.py
@@ -0,0 +1,26 @@
+options = {
+    "main": """
+    Bienvenido al Simulador de Gremio de Aventureros!
+    Seleccione una opción:
+    1. Registrar Aventurero
+    2. Registrar Misión
+    3. Realizar Misión
+    4. Otras Consultas
+    5. Salir
+    """,
+    "end": """
+    Hasta la proxima!
+    """,
+    "otherOptions": """
+    1. Ver Top 10 Aventureros con Más Misiones Resueltas
+    2. Ver Top 10 Aventureros con Mayor Habilidad
+    3. Ver Top 5 Misiones con Mayor Recompensa
+    5. Volver al Menú Principal
+    """,
+    "selectGuerrero": """
+    Elija la clase del aventurero:
+    1. Guerrero
+    2. Mago
+    3. Ranger
+    """
+}
